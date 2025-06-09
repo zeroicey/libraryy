98d71b4924b8a62e9ee1f031b8868481d85bd98f
zeroicey <zeroicey.hp@outlook.com>
2025-06-10 01:01:10 +0800

    feat(数据库): 添加电子书表结构
    新增electric_books表用于存储电子书信息，包含book_id关联书籍和url存储电子书地址

--- sql/create.sql
+++ sql/create.sql
@@ -35,6 +35,15 @@ create table if not exists `books` (
        unique key `title_unique` (`title`)
 ) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci;
 
+create table if not exists `electic_books` (
+       `id` int not null auto_increment,
+       `book_id` int not null,
+       `url` varchar(255) not null,
+       `created_at` datetime default current_timestamp,
+       `updated_at` datetime default current_timestamp on update current_timestamp,
+
+       primary key (`id`),
+)
 
 create table if not exists `borrow_records` (
     `id` int not null auto_increment,
